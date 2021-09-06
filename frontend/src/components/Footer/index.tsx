3a1345b5fd5da1fc33b0a935935a4101e8f1db47
Luis Daniel Assulfi <lassulfi@gmail.com>
2021-09-06 20:49:26 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+// @flow 
+import * as React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/lassulfi" target="_blank" rel="noreferrer">Luis Daniel Assulfi</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
