57e6a185c96dc459e67302465f93020a0bccadcf
Luis Daniel Assulfi <luis.assulfi@sensedia.com>
2021-09-10 14:10:12 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,5 @@
+import { Seller } from "./seller"
+
 export type SaleSum = {
     name: string;
     sum: number;
@@ -7,4 +9,25 @@ export type SaleSuccess = {
     name: string;
     visited: number;
     deals: number;
+}
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
 }
