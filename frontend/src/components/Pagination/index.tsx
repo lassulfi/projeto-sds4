51704c1b6b3ae95130ce3d314054b5af89e8e188
Luis Daniel Assulfi <luis.assulfi@sensedia.com>
2021-09-10 17:24:04 -0300

    Pagination

---
+++ frontend/src/components/Pagination/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { SalePage } from 'types/sale';
+
+type PaginationProps = {
+    page: SalePage;
+    onPageChange: Function;
+}
+
+const Pagination: React.FC<PaginationProps> = (props) => {
+    const { page, onPageChange } = props;
+
+    return (
+        <div className="row d-flex justify-content-center">
+            <nav>
+                <ul className="pagination">
+                    <li className={`page-item ${page.first ? 'disabled' : ''}`}>
+                        <button className="page-link" onClick={() => onPageChange(page.number - 1)}>
+                            Anterior
+                        </button>
+                    </li>
+                    <li className="page-item disabled">
+                        <span className="page-link">{page.number + 1}</span>
+                    </li>
+                    <li className={`page-item ${page.last ? 'disabled' : ''}`}>
+                        <button className="page-link" onClick={() => onPageChange(page.number + 1)}>
+                            Próxima
+                        </button>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Pagination;
